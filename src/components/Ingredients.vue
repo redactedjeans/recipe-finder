51546f43c9610997fc0e1abf28b35efb544ecf2a
Thomas Jean Lynch <tholynch@gmail.com>
2018-08-03 10:45:44 -0400

    ingredient list now keeps track of amount, +/- via button

--- src/components/Ingredients.vue
+++ src/components/Ingredients.vue
@@ -1,26 +1,55 @@
 <template>
-<div id='ingredients' class='col'>
-    <ul>
-        <li v-for="ingr in ingredients">{{ ingr.name }}</li>
-    </ul>
+<div id='ingredients' class='col col-l'>
+  <input value="search" class='search'> <!-- FIXME/TODO (search/filter/autocomplete?) -->
+
+  <ul class='ingr-list'>
+    <li v-for="ingr in ingredients">
+      <button v-on:click="add(ingr, -1)" class='btn btn-min'>-</button
+      <span class='ingr'>{{ ingr.name }}</span>
+      <span class='counter'>{{ ingr.amt }}</span>
+      <button v-on:click="add(ingr)" class='btn btn-pls'>+</button>
+      <!-- TODO when clicking +/-, push number of ingr up to App.vue -->
+    </li>
+  </ul>
 </div>
 </template>
 
 <script>
-var json = require('../assets/food.json');
+var food = require('../assets/food.json');
+// set the initial amount of every ingredient to zero
+food.map(function(f) { f.amt = 0; });
+console.log(JSON.parse(JSON.stringify(food)));
 
 export default {
   name: 'ingredients',
   data() {
     return {
-      ingredients: json
+      ingredients: food
+    }
+  },
+  methods: {
+    add: function(ingr, mult=1) {
+      // 'add' only if going up or if there's room to go down
+      if (mult>=0 || ingr.amt>0) {
+        ingr.amt += mult;
+      }
     }
   }
 }
 </script>
 
 <style lang="stylus">
-#ingredients
+.col-l
     background-color lightgreen
     flex 0 0 30%
+
+.search
+    width 100%
+
+.ingr-list
+    padding 0
+    margin 0
+    list-style none
+.btn-pls    // button +
+    float right
 </style>
