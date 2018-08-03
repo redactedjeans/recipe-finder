6bb9840034e06dc8b4e78c1f5d8f3516127d430a
Thomas Jean Lynch <tholynch@gmail.com>
2018-08-03 11:35:21 -0400

    (see previous commit)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <div id="app" class='container'>
-    <ingredients></ingredients>
-    <recipes></recipes>
+    <ingredients :ingredients="ingredients"></ingredients>
+    <recipes :recipes="recipes" :ingredients="ingredients"></recipes>
 </div>
 </template>
 
@@ -9,11 +9,30 @@
 import Ingredients from './components/Ingredients.vue'
 import Recipes from './components/Recipes.vue'
 
+// get all recipes
+var recipes = require('./assets/food.json');
+// parse recipe list to create ingredient list
+var ingredients = [];
+recipes.forEach(function(recipe) {
+  recipe.ingredients.forEach(function(ingredient) {
+    if (!ingredients.includes(ingredient)) {  // FIXME!!!
+      ingredients.push({"name": ingredient.name, "amt":0});
+    }
+  });
+});
+// console.log(JSON.parse(JSON.stringify(ingredients)));
+
 export default {
   name: 'app',
   components: {
     Ingredients,
     Recipes,
+  },
+  data() {
+    return {
+      recipes: recipes,
+      ingredients: ingredients
+    }
   }
 }
 </script>
@@ -24,17 +43,16 @@ export default {
 // @import url('https://fonts.googleapis.com/css?family=Cutive+Mono|Roboto:100')
 
 // FIXME
+// reset
 *
     box-sizing border-box
-
-// reset
 body
     margin 0
+
 // app
 .container
     background-color pink
     display flex
-
 .col
     padding 10px
 </style>
