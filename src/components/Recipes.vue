bfa0b0d72685e2861c59fb15f5fb2390ac62eef4
Thomas Jean Lynch <tholynch@gmail.com>
2018-08-03 21:36:07 -0400

    changed have_ingredients() to check ingredients recursively

--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -7,24 +7,42 @@
 </template>
 
 <script>
-
 export default {
   name: 'recipes',
   props: {
     ingredients: Object,
     recipes: Array
   },
+  data() {
+    return {
+      // get flat list of recipe names
+      r_names: this.recipes.map(r => r.name)
+    }
+  },
   methods: {
+    // TODO: find recipes that are *almost* craftable (one/two ingredients off)
+    // FIXME: if need ingredient twice, once in dependency, only checks once
+    //    e.g. cheeseburger needs tomato and relish (which needs tomato); but if
+    //         you just have one tomato it still returns true
     have_ingredients: function(recipe) {
-      // TODO: find recipes that have dependencies (eg, have wheat but not bread => can make wheat->bread->cheeseburger)
-      // TODO: find recipes that are *almost* craftable (one/two ingredients off)
       var have = true;
-      // var ingredients = this.ingredients;
       for (name in recipe.ingredients) {
-        if (this.ingredients[name] < recipe.ingredients[name]) {
-          have = false;
+        console.log('-', name);
+        if (!this.r_names.includes(name)) {
+          // ingredient is produce or meat: check ingredient list
+          have = have && (this.ingredients[name] >= recipe.ingredients[name]);
+        } else {
+          // ingredient is a recipe: check ingredient list; if enough, do nothing
+          if (this.ingredients[name] < recipe.ingredients[name]) {
+            // otherwise get recipe and check recursively
+            var next_recipe = this.recipes.filter(function(r) {
+              return r.name == name;
+            })[0];
+            have = have && this.have_ingredients(r);
+          }
         }
       }
+      // return result
       return have;
     }
   }
